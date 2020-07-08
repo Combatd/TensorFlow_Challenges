a5baa2efddf642f342429b47210fc291ca1a9ae5
Combatd <combatdstar@gmail.com>
2020-07-08 16:43:42 -0700

    GET dataset using pandas

--- corealgos.py
+++ corealgos.py
@@ -9,3 +9,8 @@ from six.moves import urllib
 import tensorflow as tf
 
 # import tensorflow.compat.v2.feature_column as fc
+
+dftrain = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/train.csv')
+dfeval = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/eval.csv')
+y_train = dftrain.pop('survived')
+y_eval = dfeval.pop('survived')
