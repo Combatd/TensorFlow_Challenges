41425410c22a84a57c45bf085e7644738117969e
Combatd <combatdstar@gmail.com>
2020-07-08 17:30:22 -0700

    Analyze training data using #describe

--- corealgos.py
+++ corealgos.py
@@ -10,7 +10,15 @@ import tensorflow as tf
 
 # import tensorflow.compat.v2.feature_column as fc
 
-dftrain = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/train.csv')
-dfeval = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/eval.csv')
+dftrain = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/train.csv') # training data
+dfeval = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/eval.csv') # testing data
+
+print(dftrain.head()) # print out pandas data frame
+
 y_train = dftrain.pop('survived')
 y_eval = dfeval.pop('survived')
+
+print(dftrain.head())
+# print(dftrain.loc[0], y_train.loc[0])
+
+dftrain.describe() # statistical analysis of our data
