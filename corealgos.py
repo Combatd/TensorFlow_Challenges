485168f559b2febb44e3f0131bbc5b640705a10d
Combatd <combatdstar@gmail.com>
2020-07-10 08:58:38 -0700

    Define Input Function for Testing Process

--- corealgos.py
+++ corealgos.py
@@ -42,3 +42,25 @@ for feature_name in NUMERIC_COLUMNS:
     feature_columns.append(tf.feature_column.numeric_column(feature_name, dtype=tf.float32))
 
 print(feature_columns)
+
+# epoch: the number of items the model will see the same data
+# For massive datasets, we need to load it in batches
+# overfitting: we pass too much data to the model and it memorizes the data points
+# We should start with a lower amount of epochs and incrementally increase batches
+
+# Input Function: the way we define data is gonna be broken into epochs
+# TensorFlow model data comes in as a tf.data.Dataset object
+def make_input_fn(data_df, label_df, num_epochs=10, shuffle=True, batch_size=32):
+    def input_function():
+        ds = tf.data.Dataset.from_tensor_slices((dict(data_df), label_df)) # create tf.data/Dataset object with data
+        if shuffle:
+            ds = ds.shuffle(1000) # randomize order of data
+        ds = ds.batch(batch_size).repeat(num_epochs) # split dataset into batches of 32 and repeat process for number of epochs
+        return ds # return a batch of the dataset
+    return input_function # return a function object for use
+
+train_input_fn = make_input_fn(dftrain, y_train)
+eval_input_fn = make_input_fn(dfeval, y_eval, num_epochs=1, shuffle=False)
+
+# estimators: basic implementations of algorithms
+linear_est = tf.estimator.LinearClassifier(feature_columns=feature_columns)
