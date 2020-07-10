bd9517725aa15d74d228d483d10df880e22e622e
Combatd <combatdstar@gmail.com>
2020-07-10 06:26:27 -0700

    Invoke TensorFlow Core Learning Algorithms

--- corealgos.py
+++ corealgos.py
@@ -22,3 +22,23 @@ print(dftrain.head())
 # print(dftrain.loc[0], y_train.loc[0])
 
 dftrain.describe() # statistical analysis of our data
+
+# Categorical data: Any data that is not numeric
+# We encode data like gender as numerical data - 0 and 1
+# 1st, 2nd, 3rd class could be 0, 1, 2....
+
+# dftrain["sex"].unique() # get unique values of sex - M, F
+
+CATEGORICAL_COLUMNS = ['sex' 'n_siblings_spouses', 'parch', 'class', 'deck', 'embark_town', 'alone']
+NUMERIC_COLUMNS = ['age', 'fare']
+
+# need to make feature_columns for linear regression
+feature_columns = []
+for feature_name in CATEGORICAL_COLUMNS:
+    vocabulary = dftrain[feature_name].unique() # get a list of all unique values from ggiven feature column
+    feature_columns.append(tf.feature_column.categorical_column_with_vocabulary_list(feature_name, vocabulary))
+
+for feature_name in NUMERIC_COLUMNS:
+    feature_columns.append(tf.feature_column.numeric_column(feature_name, dtype=tf.float32))
+
+print(feature_columns)
